0c4253070ead639073c8684ef72b6a25fceafefd
Travis Nesbit <tnesbit450@gmail.com>
2017-10-22 18:44:48 -0700

    Tested BloodPressureStaging

--- GeekMDSuite.Interpretation.Test/BloodPressureStagingTest.cs
+++ GeekMDSuite.Interpretation.Test/BloodPressureStagingTest.cs
@@ -5,10 +5,22 @@ namespace GeekMDSuite.Interpretation.Test
     public class BloodPressureStagingTest
     {
         [Fact]
-        void ReturnsCorrectStage()
+        void InterpretsPrehypertensionCorrectly()
         {
-            var bpStage = GeekMDSuite.Interpretation.BloodPressureStage.Interpret(133, 69, false);
+            var bpStage = BloodPressureStage.Interpret(133, 69, false);
             Assert.Equal(BloodPressureStages.PreHypertension, bpStage);
+        }
+        [Fact]
+        void InterpretsHypertensiveEmergencyCorrectly()
+        {
+            var bpStage = BloodPressureStage.Interpret(200, 99, true);
+            Assert.Equal(BloodPressureStages.HypertensiveEmergency, bpStage);
+        }
+        [Fact]
+        void DistinguishesHypertensiveUrgencyFromHypertensiveEmergencyCorrectly()
+        {
+            var bpStage = BloodPressureStage.Interpret(200, 99, false);
+            Assert.Equal(BloodPressureStages.HypertensiveUrgency, bpStage);
         }
     }
 }
