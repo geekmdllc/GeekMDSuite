dbdf0b5e91d05b68e733b5a25e9cdb3444ce6813
Travis Nesbit <tnesbit450@gmail.com>
2017-10-23 09:47:49 -0700

    Organized text for easier viewing

--- GeekMDSuite.Contracts/Procedures/ITreadmillExerciseStressTest.cs
+++ GeekMDSuite.Contracts/Procedures/ITreadmillExerciseStressTest.cs
@@ -4,9 +4,9 @@
     {
         TreadmillExerciseStressTestProtocol Protocol { get; }
         TreadmillExerciseStressTestResultFlag  Flag { get; }
-        int Minutes { get;}
+        int Minutes { get; }
         int Seconds { get;}
-        int HeartRateResting { get; }
+        double HeartRateResting { get; }
         double HeartRateMaxReached { get; }
         double HeartRateMaxPredicted { get; }
         string ClinicianComments { get; }
