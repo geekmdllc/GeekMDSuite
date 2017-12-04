7140be75f648513b5bdadc6170bf9a209d5b46ba
Travis Nesbit <tnesbit450@gmail.com>
2017-12-04 00:20:28 -0800

    Addded a feature for classifying the carotid utlrasound result

--- GeekMDSuite/Services/Interpretation/CarotidUltrasoundInterpretation.cs
+++ GeekMDSuite/Services/Interpretation/CarotidUltrasoundInterpretation.cs
@@ -3,14 +3,41 @@ using GeekMDSuite.Procedures;
 
 namespace GeekMDSuite.Services.Interpretation
 {
-    public class CarotidUltrasoundInterpretation : IInterpretable<CarotidPercentStenosisGrade>
+    public class CarotidUltrasoundInterpretation : IInterpretable<CarotidUltrasoundInterpretationResult>
     {
+        private readonly CarotidUltrasound _carotidUltrasound;
+
         public CarotidUltrasoundInterpretation(CarotidUltrasound carotidUltrasound)
         {
-            
+            _carotidUltrasound = carotidUltrasound;
         }
 
         public InterpretationText Interpretation => throw new NotImplementedException();
-        public CarotidPercentStenosisGrade Classification { get; }
+        
+        public CarotidUltrasoundInterpretationResult Classification => ClassifyCarotidUltrasound();
+
+        private CarotidUltrasoundInterpretationResult ClassifyCarotidUltrasound()
+        {
+            var laterality = GetLaterality();
+            var grade = GetStenosisGrade();
+
+            return new CarotidUltrasoundInterpretationResult(laterality, grade);
+        }
+
+        private CarotidPercentStenosisGrade GetStenosisGrade()
+        {
+            if (_carotidUltrasound.Left.Stenosis > _carotidUltrasound.Right.Stenosis)
+                return _carotidUltrasound.Left.Stenosis;
+            return _carotidUltrasound.Left.Stenosis < _carotidUltrasound.Right.Stenosis 
+                ? _carotidUltrasound.Right.Stenosis : _carotidUltrasound.Left.Stenosis;
+        }
+
+        private Laterality GetLaterality()
+        {
+            if (_carotidUltrasound.Left.Stenosis > _carotidUltrasound.Right.Stenosis)
+                return Laterality.Left;
+            return _carotidUltrasound.Left.Stenosis < _carotidUltrasound.Right.Stenosis 
+                ? Laterality.Right : Laterality.Bilateral;
+        }
     }
 }
---
+++ GeekMDSuite/Services/Interpretation/CarotidUltrasoundInterpretationResult.cs
@@ -0,0 +1,16 @@
+﻿using GeekMDSuite.Procedures;
+
+namespace GeekMDSuite.Services.Interpretation
+{
+    public class CarotidUltrasoundInterpretationResult
+    {
+        public CarotidUltrasoundInterpretationResult(Laterality laterality, CarotidPercentStenosisGrade grade)
+        {
+            Laterality = laterality;
+            Grade = grade;
+        }
+
+        public Laterality Laterality { get; }
+        public CarotidPercentStenosisGrade Grade { get; }
+    }
+}
