8c6ebcf7739250c9a636d816c92ac0ec65aa2e1c
Travis Nesbit <tnesbit450@gmail.com>
2017-12-07 12:32:19 -0800

    Starting central blood pressure

---
+++ GeekMDSuite/Procedures/CentralBloodPressure.cs
@@ -0,0 +1,40 @@
+﻿namespace GeekMDSuite.Procedures
+{
+    public class CentralBloodPressure
+    {
+        public CentralBloodPressure(
+            double systolicPressure, 
+            double pulsePressure, 
+            double augmentedPressure, 
+            double augmentedIndex, 
+            double referenceAge, 
+            double pulseWaveVelocity)
+        {
+            SystolicPressure = systolicPressure;
+            PulsePressure = pulsePressure;
+            AugmentedPressure = augmentedPressure;
+            AugmentedIndex = augmentedIndex;
+            ReferenceAge = referenceAge;
+            PulseWaveVelocity = pulseWaveVelocity;
+        }
+
+        public double SystolicPressure { get; }
+        public double PulsePressure { get; }
+        public double AugmentedPressure { get; }
+        public double AugmentedIndex { get; }
+        public double ReferenceAge { get;  }
+        public double PulseWaveVelocity { get;  }
+    }
+
+    public class CentralBloodPressureInterpretation
+    {
+        public CentralBloodPressureInterpretation(CentralBloodPressure centralBloodPressure, IPatient patient)
+        {
+            _centralBloodPressure = centralBloodPressure;
+            _patient = patient;
+        }
+
+        private CentralBloodPressure _centralBloodPressure;
+        private IPatient _patient;
+    }
+}
