8119a44391f0bf77857bc2a12d2acb29233f96f2
geekmdio <geekmd.io@gmail.com>
2017-12-28 00:17:42 -0800

    Mocked up test patient database. Exploring Postman. No real work

--- GeekMDSuite.WebAPI/Controllers/PatientsController.cs
+++ GeekMDSuite.WebAPI/Controllers/PatientsController.cs
@@ -1,74 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Mime;
-using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using System.Linq;
+using GeekMDSuite.WebAPI.TestRepositories;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace GeekMDSuite.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [Produces("application/json")]
-    public class ValuesController : Controller
+    public class PatientsController : Controller
     {
-        // GET api/values
+        private readonly IPatientRepo _testPatientRepo;
+
+        public PatientsController(IPatientRepo testPatientRepo)
+        {
+            _testPatientRepo = testPatientRepo;
+        }
+        // GET api/patients
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Patient> Get()
         {
-            var patient = PatientBuilder.Initialize()
-                .SetDateOfBirth(1983, 6, 29)
-                .SetGender(GenderIdentity.Male)
-                .SetMedicalRecordNumber("1234567890")
-                .SetName("Joe", "Blow")
-                .SetRace(Race.White)
-                .Build();
-
-            var serializedPatient = JsonConvert.SerializeObject(patient);
-            Console.WriteLine(serializedPatient);
-            
-            var patient2 = PatientBuilder.Initialize()
-                .SetDateOfBirth(1905, 3, 13)
-                .SetGender(GenderIdentity.Female)
-                .SetMedicalRecordNumber("0987654321")
-                .SetName("Jane", "Doe")
-                .SetRace(Race.BlackOrAfricanAmerican)
-                .Build();
-
-            var serializedPatient2 = JsonConvert.SerializeObject(patient2);
-            Console.WriteLine(serializedPatient2);
-
-            var list = new List<string>()
-            {
-                serializedPatient,
-                serializedPatient2
-            };
-            
-            return list;
+            return _testPatientRepo.All;
         }
 
-        // GET api/values/5
+        // GET api/patients/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Patient Get(int id)
         {
-            return "value";
+            return _testPatientRepo.All.ElementAt(id);
         }
 
-        // POST api/values
+        // POST api/patients
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/patients/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/patients/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            Console.WriteLine($"Removing element at {id}.");
         }
+        
+        
     }
 }
--- GeekMDSuite.WebAPI/TestRepositories/IPatientRepo.cs
+++ GeekMDSuite.WebAPI/TestRepositories/IPatientRepo.cs
@@ -4,6 +4,8 @@ namespace GeekMDSuite.WebAPI.TestRepositories
 {
     public interface IPatientRepo
     {
-        List<Patient> Repo();
+        List<Patient> All { get; }
+        List<Patient> SearchByName(string name);
+        Patient SearchByMrn(string mrn);
     }
 }
