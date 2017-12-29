231b4883bc57440a68d46904dba6ed8bd0e10525
geekmdio <geekmd.io@gmail.com>
2017-12-28 18:36:19 -0800

    Audiogram working

--- GeekMDSuite.WebAPI/Controllers/AudiogramsController.cs
+++ GeekMDSuite.WebAPI/Controllers/AudiogramsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GeekMDSuite.WebAPI.Models;
 using GeekMDSuite.WebAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -21,12 +22,73 @@ namespace GeekMDSuite.WebAPI.Controllers
            return _unitOfWork.Audiograms.All();
         }
         
+        // GET api/audiograms/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var found = _unitOfWork.Audiograms.FindById(id);
+            if (found == null) return NotFound();
+            
+            return Ok(found);
+        }
+        
         // POST api/audiograms/
         [HttpPost]
         public void Post([FromBody] AudiogramEntity audiogram)
         {
             _unitOfWork.Audiograms.Add(audiogram);
             _unitOfWork.Complete();
+        }
+        
+        // PUT api/audiograms/
+        [HttpPut]
+        public ActionResult Put([FromBody] AudiogramEntity audiogram)
+        {
+            try
+            {
+                _unitOfWork.Audiograms.Update(audiogram);
+                _unitOfWork.Complete();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+        
+        // DELETE api/audiograms/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _unitOfWork.Audiograms.Delete(id);
+                _unitOfWork.Complete();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+        
+        // DELETE/api/audiograms --> from body [1,2,3,...,n]
+        [HttpDelete]
+        public ActionResult Delete([FromBody] int[] ids)
+        {
+            try
+            {
+                foreach (var id in ids)
+                {
+                    _unitOfWork.Audiograms.Delete(id);
+                }
+                _unitOfWork.Complete();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
         }
         
         private readonly IUnitOfWork _unitOfWork;
